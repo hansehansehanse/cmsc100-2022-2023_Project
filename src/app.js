691541129920b8496750ac987793b921e590a71b
hahernandez <hahernandez@up.edu.ph>
2023-01-26 06:17:29 +0000

    feat: added openapi glue and swagger

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import Fastify from 'fastify';
 
 import sensible from '@fastify/sensible';
 
-import { general } from './services/general/index.js';
+// import { general } from './services/general/index.js';
 
-import { createBlog } from './services/blogs/create-blog.js';
-import { getManyBlog } from './services/blogs/get-many-blog.js';
-import { getBlog } from './services/blogs/get-blog.js';
+// import { createBlog } from './services/blogs/create-blog.js';
+// import { getManyBlog } from './services/blogs/get-many-blog.js';
+// import { getBlog } from './services/blogs/get-blog.js';
 
-import { updateBlog } from './services/blogs/update-blog.js';
-import { deleteBlog } from './services/blogs/delete-blog.js';
+// import { updateBlog } from './services/blogs/update-blog.js';
+// import { deleteBlog } from './services/blogs/delete-blog.js';
+
+import openAPIGlue from 'fastify-openapi-glue';
+import swagger from '@fastify/swagger';
 
 const prefix = '/api';
 
@@ -17,23 +20,36 @@ export async function build () {
   const fastify = Fastify({ logger: true }); // initialization
   fastify.register(sensible);
 
-  fastify.get(prefix, general);
-  // check app.js if errors occur
+  const openAPIGlueOptions = {
+    prefix 
+  };
 
-  // create a blog post
-  fastify.post(`${prefix}/blog`, createBlog); // create-blogs.js
+  const swaggerOptions = {
+    exposeRoute: true
+  };
 
-  // get many blog post
-  fastify.get(`${prefix}/blog`, getManyBlog); // get-many-blog.js
+  fastify.register(swagger, swaggerOptions);
 
-  // get single blog post
-  fastify.get(`${prefix}/blog/:blogId`, getBlog); // get-blog.js         //:blogId... a path not a var
+  fastify.register(openAPIGlue, openAPIGlueOptions);
+  
 
-  // update a blog post
-  fastify.put(`${prefix}/blog/:blogId`, updateBlog);
+  // fastify.get(prefix, general);
+  // // check app.js if errors occur
 
-  // delete a blog post
-  fastify.delete(`${prefix}/blog/:blogId`, deleteBlog);
+  // // create a blog post
+  // fastify.post(`${prefix}/blog`, createBlog); // create-blogs.js
+
+  // // get many blog post
+  // fastify.get(`${prefix}/blog`, getManyBlog); // get-many-blog.js
+
+  // // get single blog post
+  // fastify.get(`${prefix}/blog/:blogId`, getBlog); // get-blog.js         //:blogId... a path not a var
+
+  // // update a blog post
+  // fastify.put(`${prefix}/blog/:blogId`, updateBlog);
+
+  // // delete a blog post
+  // fastify.delete(`${prefix}/blog/:blogId`, deleteBlog);
 
   return fastify;
 }
