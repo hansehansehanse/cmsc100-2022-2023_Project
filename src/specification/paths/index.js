1298d895a0363f36ec60c25f07651bbe1138417c
hahernandez <hahernandez@up.edu.ph>
2023-01-26 09:41:31 +0000

    feat: reafactored everything

--- src/specification/paths/index.js
+++ src/specification/paths/index.js
@@ -1,7 +1,7 @@
-import { general } from "./general/index.js"
-import { blog } from "./blog/index.js";
+import { general } from './general/index.js';
+import { blog } from './blog/index.js';
 
 export const paths = {
-    ...general,
-    ...blog
-}
+  ...general,
+  ...blog
+};
